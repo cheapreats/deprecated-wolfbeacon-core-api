ee1af1c2636a47fa372c8e5fefc9b7041359dab0
bholagabbar <dev.shreyans96@gmail.com>
2017-05-23 01:05:40 +0530

    Added apidoc API documentation generation

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ WORKDIR /code
 
 RUN npm install
 
+RUN node_modules/apidoc/bin/apidoc -i routes/
+
 EXPOSE 3000
 
 CMD ["node_modules/.bin/babel-node", "./bin/www"]
--- routes/hackathon-routes.js
+++ routes/hackathon-routes.js
@@ -4,9 +4,28 @@ import HackathonValidation from '../validations/hackathon-validation';
 import HackathonController from '../controllers/hackathon-controller';
 const router = express.Router();
 
+/**
+ * @api {post} /hackathons/create Create Hackathon
+ * @apiGroup Hackathons
+ * @apiVersion 0.0.1
+ * @apiDescription Create a Hackathon in the System
+ *
+ * @apiParam {String} hackathonId   Hackathon Id
+ * @apiParam {String} hackathonUuid Hackathon UUID
+ * @apiParam {Object} hackathonData Hackathon Data as JSON Object
+ * @apiParam {String} UserId        Auth0 User ID.
+ */
 router.route('/create')
     .post(validate(HackathonValidation.createHackathonValidation), HackathonController.createHackathonController);
 
+/**
+ * @api {get} /hackathons/:id Retrieve Hackathon Details
+ * @apiGroup Hackathons
+ * @apiVersion 0.0.1
+ * @apiDescription Fetch details for a particular hackathon in the System using the Hackathon Id (hackathonId) of that particular Hackathon.
+ *
+ * @apiParam {Number} hackathonId Hackathon ID.
+ */
 router.route('/fetch?')
     .get(HackathonController.fetchHackathonDetailsController);
 
